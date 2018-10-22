9ef7a75dcdead3b464f2853748badd2e69d22850
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-22 18:42:29 +0800

    Using yargs module

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,29 +1,26 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-// var user = os.userInfo();
+const argv = yargs.argv;
+// var command = process.argv[2];
+var command = argv._[0];
+console.log('Command: ', command);
+// console.log('Process: ', process.argv);
+// console.log('Yargs: ', argv);
 
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
-
-var filteredArray = _.uniq([1,2,3,1,1,2,3,1,3,1])
-console.log(filteredArray);
-
-// var res = notes.addNote();   
-// console.log(res);
-
-// console.log('Result:', notes.add(7,-2));
-
-// option 1
-// fs.appendFileSync('greetings.txt', 'Hello world!');
-
-// option 2 
-// fs.appendFile('greetings.txt',  `Hello ${user.username}! You are ${notes.age}.`, function(err){
-// 	if (err) {
-// 		console.log('Unable to write to file');
-// 	}
-// });
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+}  else if (command === 'remove') {
+    notes.removeNote(argv.title);
+}  else {
+    console.log('Command not recognized');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,11 +1,26 @@
 console.log('Starting notes.js');
 
-module.exports.age = 25;
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note';
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
 };
 
-module.exports.add = (a, b) => {
-	return a + b;
+var getAll = () => {
+	console.log('Getting all notes');
 };
+
+var getNote = (title) => {
+	console.log('Reading note ', title);
+};
+
+var removeNote = (title) => {
+	console.log('Removing note ', title);
+};
+
+module.exports = {
+	addNote, 
+	getAll,
+	getNote,
+	removeNote,
+};
+
+
