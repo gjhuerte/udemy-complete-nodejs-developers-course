e18c07028baf984c964ed014b54aa9f0960ca242
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-22 14:16:44 +0800

    Using 3rd party modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,14 +2,21 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 // var user = os.userInfo();
 
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
+
+var filteredArray = _.uniq([1,2,3,1,1,2,3,1,3,1])
+console.log(filteredArray);
+
 // var res = notes.addNote();   
 // console.log(res);
 
-console.log('Result:', notes.add(7,-2));
+// console.log('Result:', notes.add(7,-2));
 
 // option 1
 // fs.appendFileSync('greetings.txt', 'Hello world!');
