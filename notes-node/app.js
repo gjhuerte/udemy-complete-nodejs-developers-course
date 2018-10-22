322ec680aa2ee0ab63fb164043790ef3b1f78cf6
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-22 11:24:00 +0800

    Requiring files and using module exports

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,15 +2,19 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+// var user = os.userInfo();
+
+var res = notes.addNote();
+console.log(res);
 
 // option 1
 // fs.appendFileSync('greetings.txt', 'Hello world!');
 
 // option 2 
-fs.appendFile('greetings.txt',  `Hello ${user.username}!`, function(err){
-	if (err) {
-		console.log('Unable to write to file');
-	}
-});
+// fs.appendFile('greetings.txt',  `Hello ${user.username}! You are ${notes.age}.`, function(err){
+// 	if (err) {
+// 		console.log('Unable to write to file');
+// 	}
+// });
---
+++ notes-node/notes.js
@@ -0,0 +1,7 @@
+console.log('Starting notes.js');
+
+module.exports.age = 25;
+module.exports.addNote = () => {
+	console.log('addNote');
+	return 'New note';
+};
