276a1724ee85dc53e25dc5425fc5ef08e9480850
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-24 07:18:56 +0800

    Refactor notes.js fetching notes and saving notes functionality

--- notes-node/app.js
+++ notes-node/app.js
@@ -14,7 +14,16 @@ console.log('Command: ', command);
 // console.log('Yargs: ', argv);
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+
+    if(note) {
+        console.log('Note created');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Note title taken');
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,30 +2,36 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		return JSON.parse(notesString);
+	} catch(e) {
+		return [];
+	}
+};
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 var addNote = (title, body) => {
-	var notes = [];
+	var notes = fetchNotes();
 	var note = {
 		title, body
 	};
-
-	try {
-		var notesString = fs.readFileSync('notes-data.json');
-		notes = JSON.parse(notesString);
-	} catch(e) {}
 
 	var duplicateNotes = notes.filter((note) => note.title === title);
 
 	if (duplicateNotes.length == 0) {
 		notes.push(note);
-		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-	} else {
-		console.log('Duplicate note!');
-	}
+		saveNotes(notes);
+		return note;
+	} 
 };
 
 var getAll = () => {
-	var notes = fs.readFileSync('notes-data.json');
-	return JSON.parse(notesString);
+	
 };
 
 var getNote = (title) => {
