90b34e2a808478392c461bc76ce7787ca10b33e1
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-23 15:48:14 +0800

    Add note functionality

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,11 +1,31 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-	console.log('Adding note', title, body);
+	var notes = [];
+	var note = {
+		title, body
+	};
+
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);
+	} catch(e) {}
+
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if (duplicateNotes.length == 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	} else {
+		console.log('Duplicate note!');
+	}
 };
 
 var getAll = () => {
-	console.log('Getting all notes');
+	var notes = fs.readFileSync('notes-data.json');
+	return JSON.parse(notesString);
 };
 
 var getNote = (title) => {
