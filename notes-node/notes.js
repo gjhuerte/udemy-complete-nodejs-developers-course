2fd0b2f1f1d10970da03ee6d6f0422838fcaf5de
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-24 08:09:33 +0800

    Debugging application using inspect

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -50,6 +50,7 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
+	debugger;
 	console.log('--');
 	console.log(`Title: ${note.title}`);
 	console.log(`Body: ${note.body}`);
---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: "Gabriel",
+};
+
+person.age = 20;
+
+debugger;
+
+person.name = "Giselle";
+
+console.log(person);
