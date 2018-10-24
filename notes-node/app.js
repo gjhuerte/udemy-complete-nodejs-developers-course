a44e215be9b0134c44453d5af52ae5f3972e8e65
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-24 17:24:24 +0800

    Arguments and advance yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,7 +6,32 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+var titleOptions = {
+    describe: "Title of note",
+    demand: true,
+    alias: 't'
+};
+
+var bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b',
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', { 
+        title: titleOptions, 
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', { 
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', { 
+        title: titleOptions
+    })
+    .help()
+    .argv;
 // var command = process.argv[2];
 var command = argv._[0];
 console.log('Command: ', command);
@@ -23,7 +48,9 @@ if (command === 'add') {
         console.log('Note title taken');
     }
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
     var note = notes.getNote(argv.title);
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -31,7 +31,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-	
+	return fetchNotes();
 };
 
 var getNote = (title) => {
