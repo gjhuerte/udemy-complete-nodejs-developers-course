d832a8904977eb794720ec4dcf781ec11f618696
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-09 20:11:40 +0800

    Fetching latitude, longitude, and formatted address from google api

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4241%20St%20Peter',
     json: true,
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
-});
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+}  );
