38f2e0ac7fabe735190b3acf81a3ed9287f45bcb
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-11 13:55:55 +0800

    Added yargs for weather application

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        },
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4241%20St%20Peter',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true,
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
